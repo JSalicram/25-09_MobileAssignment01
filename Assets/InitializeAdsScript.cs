5d63ee9b247c35b7edc33eb6d79a9cdfd3b475b4
JSalicram <JSalicram@users.github.com>
2019-09-25 20:29:19 +0100

    Initial commit

---
+++ Assets/InitializeAdsScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class InitializeAdsScript : MonoBehaviour
+{
+    public void AdButtonPressed()
+    {
+        var options = new ShowOptions { resultCallback = HandleShowResult };
+        Advertisement.Show(options);
+    }
+
+    private void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("ad finished");
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("ad skipped");
+                break;
+            case ShowResult.Failed:
+                Debug.LogError("ad failed");
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/FireCube.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCube : MonoBehaviour
+{
+    public float m_Speed = 20f;
+
+    private Vector3 m_Direction = Vector3.forward;
+
+    public void ToggleDirection()
+    {
+        Debug.Log("Toggling direction");
+
+        if (m_Direction == Vector3.forward)
+        {
+            m_Direction = Vector3.back;
+        }
+        else
+        {
+            m_Direction = Vector3.forward;
+        }
+    }
+
+    void Update()
+    {
+        transform.Translate(m_Direction * Time.deltaTime * m_Speed);
+    }
+}
